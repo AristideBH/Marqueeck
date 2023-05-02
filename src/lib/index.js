837a310bb63623ec30e2643dc3fe6d7c807b7fdc
AristideBH <aristide.bruneau@gmail.com>
2023-05-02 14:07:03 +0200

    0.0.12

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,7 @@
 // Reexport your entry components here
 import Marqueeck from "$lib/Marqueeck.svelte"
-// import { MarqueeckTranslate } from "$lib/MarqueeckTranslate.js"
+import { MarqueeckTranslate } from "$lib/MarqueeckTranslate.js"
 
 // export { Marqueeck, MarqueeckTranslate }
 export default Marqueeck
+export { MarqueeckTranslate }
