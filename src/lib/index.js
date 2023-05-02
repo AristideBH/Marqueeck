7498b9ffa035800378a00febe0dd4e0c79ee89ae
AristideBH <aristide.bruneau@gmail.com>
2023-05-02 13:50:12 +0200

    go

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,5 @@
 // Reexport your entry components here
-// import Marqueeck from "$lib/Marqueeck.svelte"
-// import { MarqueeckTranslate } from "./MarqueeckTranslate.ts"
-// export { Marqueeck, MarqueeckTranslate }
+import Marqueeck from "$lib/Marqueeck.svelte"
+import { MarqueeckTranslate } from "$lib/MarqueeckTranslate.js"
+
+export { Marqueeck, MarqueeckTranslate }
