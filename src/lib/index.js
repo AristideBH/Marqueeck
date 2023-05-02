416968aea3f6e1152b436b9e364bf2c5b2f14f5f
AristideBH <aristide.bruneau@gmail.com>
2023-05-02 12:00:04 +0200

    0.0.4 > include ts instead of js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
 // Reexport your entry components here
 import Marqueeck from "$lib/Marqueeck.svelte"
-import { MarqueeckTranslate } from "$lib/MarqueeckTranslate.ts"
+import { MarqueeckTranslate } from "$lib/MarqueeckTranslate.js"
 export { Marqueeck, MarqueeckTranslate }
