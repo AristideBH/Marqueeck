ac6737863d011bec20739972acb7162db194dd9a
AristideBH <aristide.bruneau@gmail.com>
2023-05-02 18:08:43 +0200

    0.0.26

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,99 +1,151 @@
 <script lang="ts">
 	import Marqueeck from '@arisbh/marqueeck';
 	import { CodeBlock } from '@skeletonlabs/skeleton';
-</script>
-
-<h1 class="py-4">Marqueeck demo</h1>
-<div class="space-y-2">
-	<h2 class="mb-2">Basic usage</h2>
-
-	<Marqueeck>
-		<span>Hello Marqueeck</span>
-	</Marqueeck>
-
-	<CodeBlock
-		language="svelte"
-		code={`
-	<Marqueeck>
-		<span>Hello Marqueeck</span>
-</Marqueeck>`}
-	/>
-</div>
-
-<div class="space-y-2">
-	<h2 class="mb-2">Advanced options</h2>
-
-	<h3 class="mb-2">Sticky element</h3>
-
-	<Marqueeck options={{ stickyPosition: 'end' }}>
-		<span>Hello Marqueeck</span>
-		<svelte:fragment slot="sticky">Sticky element</svelte:fragment>
-	</Marqueeck>
-
-	<CodeBlock
-		language="svelte"
-		code={`
-	<Marqueeck options={{ stickyPosition: 'end' }}>
-		<span>Hello Marqueeck</span>
-		<svelte:fragment slot="sticky">Sticky element</svelte:fragment>
-</Marqueeck>`}
-	/>
-
-	<h3 class="mb-2">Styling via CSS Variables</h3>
-
-	<Marqueeck --bg-color={'red'} --text-color={'#000'}>
-		<span>Hello Marqueeck</span>
-	</Marqueeck>
-
-	<CodeBlock
-		language="svelte"
-		code={`
-	<Marqueeck --bg-color={'red'} --text-color={"#000"}>
-		<span>Hello Marqueeck</span>
-</Marqueeck>`}
-	/>
-</div>
-
-<div class="space-y-2">
-	<h2 class="mb-2">Full options</h2>
-
-	<Marqueeck
-		options={{
-			speed: 50,
-			direction: 'right',
-			gap: 20,
-			paddingX_Wrapper: 20,
-			paddingY_Wrapper: 16,
-			debug: false,
-			onHover: 'customSpeed',
-			gradualHoverDuration: 1250,
-			hoverSpeed: 10,
-			stickyPosition: 'start'
-		}}
-	>
-		<span>Hello Marqueeck</span>
-	</Marqueeck>
-
-	<CodeBlock
-		language="svelte"
-		code={`
-<script>
-	const options = {
-		speed: 50,
-		direction: 'right',
-		gap: 20,
-		paddingX_Wrapper: 20,
-		paddingY_Wrapper: 16,
-		debug: false,
-		onHover: 'customSpeed',
-		gradualHoverDuration: 1250,
-		hoverSpeed: 10,
-		stickyPosition: 'start'
+	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+	import { Table } from '@skeletonlabs/skeleton';
+	import type { TableSource } from '@skeletonlabs/skeleton';
+	import { tableMapperValues } from '@skeletonlabs/skeleton';
+	let tabSet: number = 0;
+	const sourceData = [
+		{
+			name: 'speed',
+			type: 'number',
+			value: 50,
+			description: 'The speed of the moving content (px/sec)'
+		},
+		{
+			name: 'direction',
+			type: "'left' | 'right'",
+			value: "'left'",
+			description: 'The direction of the moving content'
+		},
+		{
+			name: 'gap',
+			type: 'number',
+			value: 20,
+			description: 'The distance between repeated elements (px)'
+		},
+		{
+			name: 'paddingX_Wrapper',
+			type: 'number',
+			value: 20,
+			description: 'The horizontal padding of the container'
+		},
+		{
+			name: 'paddingY_Wrapper',
+			type: 'number',
+			value: 16,
+			description: 'The vertical padding of the container'
+		},
+		{
+			name: 'onHover',
+			type: "'none' | 'stop' | 'customSpeed'",
+			value: "'customSpeed'",
+			description: 'The behaviour when hovering the container'
+		},
+		{
+			name: 'gradualHoverDuration',
+			type: 'number',
+			value: 1250,
+			description: "The time it takes to drop to 'customSpeed', automaticaly eased (ms)"
+		},
+		{
+			name: 'hoverSpeed',
+			type: 'number',
+			value: 10,
+			description: 'The speed of the moving content when hovering (px/sec)'
+		},
+		{
+			name: 'stickyPosition',
+			type: "'start' | 'end'",
+			value: "'start'",
+			description: 'The position of the provided sticky element'
+		}
+	];
+	const tableSimple: TableSource = {
+		// A list of heading labels.
+		head: ['Name', 'Type', 'Default value', 'Description'],
+		// The data visibly shown in your table body UI.
+		body: tableMapperValues(sourceData, ['name', 'type', 'value', 'description']),
+		// Optional: The data returned when interactive is enabled and a row is clicked.
+		meta: tableMapperValues(sourceData, ['name', 'type', 'value', 'description'])
 	};
 </script>
 
-<Marqueeck {options}>
+<svelte:head>
+	<title>Marqueeck - A custom marquee component for Sveltekit</title>
+</svelte:head>
+
+<h1 class="py-4">Marqueeck demo</h1>
+<CodeBlock
+	language="bash"
+	code={`
+pnpm i @arisbh/marqueeck`}
+/>
+
+<TabGroup>
+	<Tab bind:group={tabSet} name="tab1" value={0}>Usage</Tab>
+
+	<Tab bind:group={tabSet} name="tab2" value={1}>Props</Tab>
+
+	<Tab bind:group={tabSet} name="tab3" value={2}>Styling</Tab>
+	<Tab bind:group={tabSet} name="tab4" value={3}>Event</Tab>
+	<!-- Tab Panels --->
+	<svelte:fragment slot="panel">
+		{#if tabSet === 0}
+			<div class="space-y-2">
+				<h2 class="mb-2">Basic usage</h2>
+
+				<Marqueeck>
+					<span>Hello Marqueeck</span>
+				</Marqueeck>
+
+				<CodeBlock
+					language="svelte"
+					code={`
+	<Marqueeck>
 		<span>Hello Marqueeck</span>
 </Marqueeck>`}
-	/>
-</div>
+				/>
+			</div>
+
+			<div class="space-y-2">
+				<h2 class="mb-2">Advanced configuration</h2>
+
+				<h3 class="mb-2">Sticky element</h3>
+
+				<Marqueeck options={{ stickyPosition: 'end' }}>
+					<span>Hello Marqueeck</span>
+					<svelte:fragment slot="sticky">Sticky element</svelte:fragment>
+				</Marqueeck>
+
+				<CodeBlock
+					language="svelte"
+					code={`
+	<Marqueeck options={{ stickyPosition: 'end' }}>
+		<span>Hello Marqueeck</span>
+		<svelte:fragment slot="sticky">Sticky element</svelte:fragment>
+</Marqueeck>`}
+				/>
+			</div>
+		{:else if tabSet === 1}
+			<Table source={tableSimple} />
+		{:else if tabSet === 2}
+			<h3 class="mb-2">Slotted component</h3>
+
+			<h3 class="mb-2">Styling via CSS Variables</h3>
+			<Marqueeck --bg-color={'red'} --text-color={'#000'}>
+				<span>Hello Marqueeck</span>
+			</Marqueeck>
+
+			<CodeBlock
+				language="svelte"
+				code={`
+		<Marqueeck --bg-color={'red'} --text-color={"#000"}>
+			<span>Hello Marqueeck</span>
+</Marqueeck>`}
+			/>
+			<h3 class="mb-2">Using custom classes</h3>
+		{/if}
+	</svelte:fragment>
+</TabGroup>
