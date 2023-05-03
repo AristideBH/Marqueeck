8b811e87729323586ca71ff6a2172974eb2eaff9
AristideBH <aristide.bruneau@gmail.com>
2023-05-03 16:54:43 +0200

    ts import no Translate fn

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,6 @@
 // Reexport your entry components here
 import Marqueeck from "./Marqueeck.svelte"
-import { MarqueeckTranslate } from "./MarqueeckTranslate.js"
+import { MarqueeckTranslate } from "./MarqueeckTranslate.ts"
 
-// export { Marqueeck, MarqueeckTranslate }
 export default Marqueeck
-export { MarqueeckTranslate }
+// export { MarqueeckTranslate }
