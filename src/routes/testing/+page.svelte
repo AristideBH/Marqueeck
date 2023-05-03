9b6c6bd4c192825c5f406700d425d562429a0aef
AristideBH <aristide.bruneau@gmail.com>
2023-05-03 18:04:03 +0200

    vup

--- src/routes/testing/+page.svelte
+++ src/routes/testing/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Marqueeck from '@arisbh/marqueeck';
-	// import type { MarqueeckOptions } from '@arisbh/marqueeck';
 	const options: MarqueeckOptions = {};
 	// $: console.log(Marqueeck);
 </script>
