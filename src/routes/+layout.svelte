9ae967866dc34868238920d3567f59edf9b3ea8f
AristideBH <aristide.bruneau@gmail.com>
2023-05-09 12:09:12 +0200

    Update docs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 
 <AppShell
 	slotPageHeader="px-4 py-8 container mx-auto flex gap-16 flex-wrap items-baseline md:flex-nowrap"
-	slotPageFooter="px-4 py-2 container mx-auto justify-between flex mt-16"
+	slotPageFooter="px-4 py-2 container mx-auto justify-between flex mt-16 gap-4"
 >
 	<svelte:fragment slot="pageHeader">
 		<div class=" flex flex-col gap-4 w-full max-w-lg">
@@ -23,7 +23,7 @@
 
 			<!-- INSTALL COMMAND -->
 			<div class="flex gap-4 w-full md:flex-col items-start">
-				<CodeBlock language="bash" code={`npm i @arisbh/marqueeck`} class="w-full" />
+				<CodeBlock language="cmd" code={`npm i @arisbh/marqueeck`} class="w-full" />
 				<a href="https://github.com/AristideBH/Marqueeck/">
 					<span class="chip variant-ghost-primary">Github page</span>
 				</a>
--- src/routes/Usage.svelte
+++ src/routes/Usage.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Marqueeck from '@arisbh/marqueeck';
+	// import Marqueeck from '@arisbh/marqueeck';
+	import Marqueeck from '$lib/index.ts';
 	import {
 		CodeBlock,
 		TabGroup,
@@ -11,84 +12,103 @@
 </script>
 
 <!-- ! Tab Panel - USAGE --->
-<div id="toc" class="flex gap-8 container flex-wrap md:flex-nowrap mt-8 w-full">
+<div id="toc" class="flex gap-16 container flex-wrap md:flex-nowrap mt-8 w-full items-start">
 	<TableOfContents
-		class="  sticky top-0 md:relative z-10 bg-surface-50-900-token"
+		class="  md:sticky top-10 z-10 bg-surface-50-900-token md:top-16 "
 		target="#toc"
 		scrollParent="#page"
 		allowedHeadings="h2, h3, h4"
 		label="Table of content"
 		width="min-w-[220px] w-full md:min-w-unset md:w-fit"
+		regionLabel="pt-4 md:!pt-0"
 	/>
-	<main class="flex flex-col">
-		<h2>hhelo</h2>
-		<h2>hhelo</h2>
-		<h2>hhelo</h2>
-		<h2>hhelo</h2>
-		<h2>hhelo</h2>
-		<section>
-			<h2>hhelo</h2>
-			<!-- <h3 class=" w-[2500px] max-w-full">SpeedFactor</h3> -->
-
-			<Marqueeck>Hello Marqueeck</Marqueeck>
-		</section>
-		<!-- <section class="space-y-2 max-w-full">
+	<main class="min-w-[220px] w-full md:min-w-unset md:w-fit space-y-8">
+		<section class="space-y-2">
 			<h2 class="mb-4">Basic usage</h2>
-
-
+			<p>
+				You can throw any element in <code>Marqueeck</code>, solo or grouped, a simple div or
+				another Svelte component, or even just plain text for the sake of simplicity.
+			</p>
+			<p>
+				Your element will automaticaly be repeated inside the component, auto-updating the number of
+				needed elements to fill its parent.
+			</p>
+			<Marqueeck>Hello Marqueeck</Marqueeck>
 			<CodeBlock
 				language="html"
 				code={`
-		<script>
-			import Marqueeck from '@arisbh/marqueeck';
-		</script>
-		
-		<Marqueeck>Hello Marqueeck</Marqueeck>
+<script>
+	import Marqueeck from '@arisbh/marqueeck';
+</script>
+
+<Marqueeck>Hello Marqueeck</Marqueeck>
 				`}
 			/>
-			<p>
-				You can throw any element in <code>Marqueeck</code>, solo or grouped, a simple div or
-				another svelte component, or even just plain text, for the sake of simplicity.
-			</p>
-			<p>
-				You element will be automaticaly repeated inside the component, auto-updating the number of
-				elements based on screen's width
-			</p>
 		</section>
 
-		<section class="space-y-2 pt-8 max-w-full">
-			<h2 class="mb-2">Advanced configuration</h2>
-			<h3 class="mb-2">Sticky element</h3>
+		<hr />
 
+		<section class="space-y-2">
+			<h2 class="mb-4">Advanced configuration</h2>
+			<h3 class="mb-2">Sticky element</h3>
+			<p>
+				You can use the reserved <code>svelte:fragment</code> to place a sticky element inside your component.
+			</p>
 			<Marqueeck>
 				Marqueeck
 				<svelte:fragment slot="sticky">
-					<strong>Hello</strong>
+					<strong>HELLO</strong>
 				</svelte:fragment>
 			</Marqueeck>
 			<CodeBlock
 				language="html"
 				code={`
-		<Marqueeck >
-			Marqueeck
-			<svelte:fragment slot="sticky">
-				<strong>Hello</strong>
-			</svelte:fragment>
-		</Marqueeck>
-				`}
+<Marqueeck >
+	Marqueeck
+	<svelte:fragment slot="sticky">
+		<strong>HELLO</strong>
+	</svelte:fragment>
+</Marqueeck>
+	`}
 			/>
-			<div class="!mt-4 max-w-3xl space-y-2">
+			<blockquote class="!mt-4">
+				Customize its placement with the <code>stickyPosition</code> key inside
+				<code>MarqueeckOptions</code>.
+			</blockquote>
+		</section>
+		<hr />
+
+		<section class="space-y-2">
+			<h3 class="mb-2">onHover</h3>
+			<p>
+				By default, Marqueeck will slow down to a predefined speed when your mouse is hovering it
+				(see previous demos).
+			</p>
+			<p>You can overide this behaviour with the <code>onHover</code> property inside options :</p>
+			<ul class="list-disc ml-6">
+				<li><code>none</code> : nothing will happen upon hover</li>
+				<li><code>stop</code> : the component speed will smoothly stops to zero.</li>
+			</ul>
+			<blockquote class="!mt-4">
 				<p>
-					You can use the reserved <code>svelte:fragment</code> to place a sticky element inside the
-					component.
+					You can customize the time it takes to stop or slow down the animation using the <code
+						>gradualHoverDuration</code
+					> proprety.
 				</p>
-				<p>Customize its placement with the <code>stickyPosition</code> key inside options.</p>
-			</div>
+				<p>
+					To edit the minimum speed when onHover is set to <code>customSpeed</code>, use the
+					<code>hoverSpeed</code> property.
+				</p>
+			</blockquote>
 		</section>
 
-		<section class="container max-w-ful">
-			<h3>Speed and onHover</h3>
-			<h3 class=" w-[2500px] max-w-full">SpeedFactor</h3>
-		</section> -->
+		<hr />
+		<section class="space-y-2">
+			<h3 class="mb-2">speedFactor</h3>
+			<p>
+				Marqueeck provide a way to live-impact it speed, through the <code>speedFactor</code> proprety.
+			</p>
+			<blockquote class="!mt-4">...writing documentation...</blockquote>
+		</section>
 	</main>
 </div>
